14cec6cec1174081d268a6f9a74bc1bd168bca11
bcsabae <58107994+bcsabae@users.noreply.github.com>
2024-08-23 16:30:11 +0200

    add question seeder

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'question_text' => fake()->sentence(),
+            'upvotes' => fake()->numberBetween(0, 100),
+            'is_answered' => false
+        ];
+    }
+}
