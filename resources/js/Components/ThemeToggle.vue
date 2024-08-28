763b148ccf2001adfa9edc76b9fd853ff3c3c8a2
bcsabae <58107994+bcsabae@users.noreply.github.com>
2024-08-28 12:57:26 +0200

    refactor to use SFC

--- resources/js/Components/ThemeToggle.vue
+++ resources/js/Components/ThemeToggle.vue
@@ -1,5 +1,34 @@
+<script setup>
+import ToggleSwitch from 'primevue/toggleswitch';
+import {onMounted, ref} from "vue";
+
+const props = defineProps({
+    reverse: Boolean
+})
+
+const dark = ref(false)
+
+function toggleTheme() {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+onMounted( () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+        dark.value = true;
+    }
+})
+</script>
+
 <template>
-    <div class="flex gap-x-4 px-4" :class="{ 'flex-row-reverse': reverse }">
+    <div class="flex gap-x-4 px-4" :class="{ 'flex-row-reverse': props.reverse }">
         <div class="text-gray-900 dark:text-gray-100 text-sm">
             <p v-if="dark">dark</p>
             <p v-else>light</p>
@@ -7,39 +36,3 @@
         <ToggleSwitch v-model="dark" @click="toggleTheme"/>
     </div>
 </template>
-
-<script>
-import ToggleSwitch from 'primevue/toggleswitch';
-
-export default {
-    data() {
-        return {
-            dark: false
-        }
-    },
-    props: {
-        reverse: Boolean
-    },
-    components: {
-        ToggleSwitch,
-    },
-    methods: {
-        toggleTheme() {
-            if (document.documentElement.classList.contains('dark')) {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem('theme', 'light');
-            } else {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('theme', 'dark');
-            }
-        },
-    },
-    mounted() {
-        const savedTheme = localStorage.getItem('theme');
-        if (savedTheme === 'dark') {
-            document.documentElement.classList.add('dark');
-            this.dark = true;
-        }
-    },
-};
-</script>
