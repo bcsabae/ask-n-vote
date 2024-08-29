f103edf11749621108224b2c317c1ce5bf293558
bcsabae <58107994+bcsabae@users.noreply.github.com>
2024-08-29 14:49:01 +0200

    move CTA to component

---
+++ resources/js/Components/CTA.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+const props = defineProps({
+    href: String,
+    text: String
+})
+</script>
+
+<template>
+    <Link :href="href" class="rounded-full bg-secondary hover:bg-primary hover:shadow transition ease-in-out duration-300 px-4 py-2 text-black font-black tracking-wider uppercase">
+        {{ text }}
+    </Link>
+</template>
--- resources/js/Layouts/LandingLayout.vue
+++ resources/js/Layouts/LandingLayout.vue
@@ -4,12 +4,15 @@ import { Head, Link, router } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import ThemeToggle from "@/Components/ThemeToggle.vue";
+import CTA from "@/Components/CTA.vue";
 
 defineProps({
     title: String,
+    hasWaves: Boolean,
 });
 
 const showingNavigationDropdown = ref(false);
+const year = new Date().getFullYear()
 
 const logout = () => {
     router.post(route('logout'));
@@ -23,7 +26,27 @@ const logout = () => {
         <Banner />
 
         <div class="flex flex-col min-h-screen mx-auto bg-white dark:bg-gray-900">
-            <nav class="lg:w-4/5 w-full px-2 mx-auto mt-4">
+
+            <div v-if="hasWaves" class="absolute z-0">
+                <div class="overflow-hidden w-screen h-96 bg-primary bg-gradient-to-b from-gray-200 dark:from-gray-800 to-primary"></div>
+                <div class="overflow-hidden w-screen">
+                    <svg class="waves z-0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                         viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                        <defs>
+                            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                        </defs>
+                        <g class="parallax">
+                            <use xlink:href="#gentle-wave" x="48" y="0" fill="#4deee8" />
+                            <use xlink:href="#gentle-wave" x="48" y="3" fill="#4deee860" />
+                            <use xlink:href="#gentle-wave" x="48" y="5" fill="#4deee880" />
+                            <use xlink:href="#gentle-wave" x="48" y="7" fill="#4deee8a0" />
+                        </g>
+                    </svg>
+                </div>
+            </div>
+
+
+            <nav class="lg:w-4/5 w-full px-2 mx-auto mt-4 z-10">
                 <!-- Primary Navigation Menu -->
                 <div class="mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
@@ -37,14 +60,14 @@ const logout = () => {
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px md:ms-10 md:flex">
-                                <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                                <Link href="#product" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
                                     Product
                                 </Link>
-                                <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                                <Link href="#features" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
                                     Features
                                 </Link>
-                                <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
-                                    Resources
+                                <Link href="#pricing" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                                    Pricing
                                 </Link>
                             </div>
                         </div>
@@ -54,9 +77,7 @@ const logout = () => {
                                 <ThemeToggle />
                             </div>
 
-                            <Link :href="route('home')" class="ms-auto rounded-full bg-secondary hover:bg-primary hover:shadow transition ease-in-out duration-300 px-4 py-2 text-black font-black tracking-wider uppercase">
-                                Get started
-                            </Link>
+                            <CTA :href="route('home')" text="Get started" />
                         </div>
 
                         <!-- Hamburger -->
@@ -91,19 +112,17 @@ const logout = () => {
                 <!-- Responsive Navigation Menu -->
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="md:hidden">
                     <div class="pt-2 px-4 pb-3 space-y-1 flex flex-col gap-y-4 mt-8 border-b border-b-gray-200">
-                        <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                        <Link href="#product" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
                             Product
                         </Link>
-                        <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                        <Link href="#features" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
                             Features
                         </Link>
-                        <Link :href="route('home')" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
-                            Resources
+                        <Link href="#pricing" class="text-gray-700 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 font-black tracking-wider uppercase">
+                            Pricing
                         </Link>
                         <div class="py-4 flex flex-row justify-between">
-                            <Link :href="route('home')" class="rounded-full bg-secondary hover:bg-primary hover:shadow transition ease-in-out duration-300 px-4 py-2 text-black font-black tracking-wider uppercase">
-                                Get started
-                            </Link>
+                            <CTA :href="route('register')" text="Get started" />
                             <div class="inline-flex items-center px-1 pt-1">
                                 <ThemeToggle :reverse="true" />
                             </div>
@@ -113,7 +132,7 @@ const logout = () => {
                 </div>
             </nav>
 
-            <div class="lg:w-4/5 w-full px-2 mx-auto flex-grow lg:my-8 my-4 rounded-xl bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-200">
+            <div class="lg:w-4/5 w-full px-2 mx-auto flex-grow lg:my-8 my-4 rounded-xl bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200">
                 <div class="px-4 py-2">
 
                     <!-- Page Content -->
@@ -122,6 +141,60 @@ const logout = () => {
                     </main>
                 </div>
             </div>
+
+            <div class="bg-gray-600 px-24 py-8 text-center">
+                © {{ year }} AskNVote | <a class="hover:underline cursor-pointer">Privacy Policy</a> | <a class="hover:underline cursor-pointer">Terms of Use</a>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+.waves {
+    position:relative;
+    width: 100%;
+    height:15vh;
+    margin-bottom:-7px;
+    min-height:100px;
+    max-height:150px;
+    transform: scaleY(-1);
+}
+
+.parallax > use {
+    animation: move-forever 25s cubic-bezier(.55,.5,.45,.5) infinite;
+}
+.parallax > use:nth-child(1) {
+    animation-delay: -2s;
+    animation-duration: 18s;
+}
+.parallax > use:nth-child(2) {
+    animation-delay: -3s;
+    animation-duration: 15s;
+}
+.parallax > use:nth-child(3) {
+    animation-delay: -4s;
+    animation-duration: 25s;
+}
+.parallax > use:nth-child(4) {
+    animation-delay: -5s;
+    animation-duration: 40s;
+}
+@keyframes move-forever {
+    0% {
+        transform: translate3d(-90px,0,0);
+    }
+    100% {
+        transform: translate3d(85px,0,0);
+    }
+}
+@media (max-width: 768px) {
+    .waves {
+        height:40px;
+        min-height:40px;
+    }
+}
+@keyframes blink {
+    from, to { border-color: transparent; }
+    50% { border-color: black; }
+}
+</style>
